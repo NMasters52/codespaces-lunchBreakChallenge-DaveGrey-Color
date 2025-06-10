b7f8d1ee9d0103421223f22b4dd9b68a630efd5e
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-06-10 16:30:11 +0000

    refractor input

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,13 @@ function App() {
       <div className="colorDisplay" style={colorDisplayStyles}>
         <p>{userInput.length ? userInput : 'Empty Value'}</p>
       </div>
-      <input autoFocus tabIndex={0} placeholder="Type Color Name" onChange={handleUserInput} className="userInput" />
+      <input 
+        autoFocus 
+        tabIndex={0} 
+        placeholder="Type Color Name" 
+        onChange={handleUserInput} 
+        className="userInput" 
+      />
     </main>
   );
 }
