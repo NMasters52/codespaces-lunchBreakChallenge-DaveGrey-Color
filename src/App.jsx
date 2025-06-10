bcb6ada3a2f1d4cf1fd435f33a281b06aeafcd61
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-06-10 15:59:52 +0000

    bug:colorDisplay displaying empty strings of the userInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 function App() {
-  const [userInput, setUserInput] = useState('Empty Value')
+  const [userInput, setUserInput] = useState('')
 
   const handleUserInput = (e) => setUserInput(e.target.value) 
 
+  const colorDisplayStyles = {
+    backgroundColor: userInput
+  }
+
   return (
     <main>
-      <div className="colorDisplay">
-        <p>{userInput}</p>
+      <div className="colorDisplay" styles={colorDisplayStyles}>
+        <p>{userInput.length ? userInput : 'Empty Value'}</p>
       </div>
       <input placeholder="Type Color Name" onChange={handleUserInput} />
     </main>
